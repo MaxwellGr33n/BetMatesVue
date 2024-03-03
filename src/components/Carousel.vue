05582ef523e82cbd7b824297b9388f781c2b4b4d
Max Green <maxdgreen@live.com.au>
2024-03-03 13:34:07 +1100

    Init

---
+++ src/components/Carousel.vue
@@ -0,0 +1,20 @@
+<template>
+    <section className="mt-12 w-full bg-neutral-100">
+        <img className="" src="//www.ladbrokes.com.au/cdn/contentful/images/J9Zlbf9VwEuTI8OiSvPEW/b7a4aac66cb484549528906dd4ee1561/LadbrokesLive_MOBILE.jpg" />
+        <div className="flex gap-2 justify-center p-2">
+            <font-awesome-icon icon="circle" />
+            <font-awesome-icon icon="circle" />
+            <font-awesome-icon icon="circle" />
+            <font-awesome-icon icon="solid circle" />
+            <font-awesome-icon icon="circle" />
+            <font-awesome-icon icon="circle" />
+        </div>
+    </section>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+    <nav class="flex justify-center fixed top-0 bg-violet-500 h-12 w-screen pl-4 z-10">
+       <div class="flex justify-between items-center w-screen sm:w-4/5 max-w-screen-lg">
+         <h1 class="font-logo italic text-2xl text-white">Betmates</h1>
+         <ul class="flex h-full">
+           <li v-for="item in navbarItems" :key="item.id" class="m-auto px-2 flex items-center border-l-[1px] border-neutral-600 shadow-inner text-white text-sm h-full ">
+             <span :class="[item.active ? 'bg-violet-400' : '']">
+               <FontAwesomeIcon v-if="item.icon" :icon="item.icon" class="px-2" />
+               {{ item.text }}
+             </span>
+           </li>
+         </ul>
+       </div>
+    </nav>
+    <div class="pt-12"></div>
+   </template>
+   
+   <script setup lang="ts">
+    import { reactive } from 'vue';
+    import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+    import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
+    const navbarItems = reactive([
+      { id: 1, text: '', icon: faMagnifyingGlass, active: false },
+      { id: 2, text: 'Login', active: false },
+      { id: 3, text: 'Register', active: false },
+    ]);
+   </script>
