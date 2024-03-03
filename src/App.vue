c2427967836d6567995d3ece031f4c7bff130fe0
Max Green <maxdgreen@live.com.au>
2024-03-04 09:29:30 +1100

    Added default styling to near root div.

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,10 @@ import NavBar from './components/NavBar.vue'
 
 <template>
   <NavBar />
-  <RouterView />
+  <div class="flex flex-col mx-auto items-center justify-center w-full sm:w-4/5 max-w-screen-lg">
+    <RouterView /> 
+  </div>
+  
 </template>
 
 <style scoped>
