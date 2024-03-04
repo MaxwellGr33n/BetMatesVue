f683c7ea2f2147b72af56bd1f78224099aa9a62e
Max Green <maxdgreen@live.com.au>
2024-03-04 15:23:54 +1100

    Add quicklinks to bottom of app

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import NavBar from './components/NavBar.vue'
+import { RouterLink, RouterView } from "vue-router";
+import NavBar from "./components/NavBar.vue";
+import QuickLinks from "./components/QuickLinks.vue";
 </script>
 
 <template>
   <NavBar />
   <div class="flex flex-col mx-auto items-center justify-center w-full sm:w-4/5 max-w-screen-lg">
-    <RouterView /> 
+    <RouterView />
   </div>
-  
+  <QuickLinks />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
