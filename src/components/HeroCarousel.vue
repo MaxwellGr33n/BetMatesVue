238cce9ccc997f0c478db2e56045f137c65ce280
Max Green <maxdgreen@live.com.au>
2024-03-04 15:22:30 +1100

    Formatting

--- src/components/HeroCarousel.vue
+++ src/components/HeroCarousel.vue
@@ -1,20 +1,21 @@
 <template>
-    <section className="w-full">
-        <img className="" src="//www.ladbrokes.com.au/cdn/contentful/images/J9Zlbf9VwEuTI8OiSvPEW/b7a4aac66cb484549528906dd4ee1561/LadbrokesLive_MOBILE.jpg" />
-        <div className="flex gap-2 justify-center p-2">
-            <Icon icon="fa6-solid:circle" />
-            <Icon icon="fa6-solid:circle" />
-            <Icon icon="fa6-regular:circle" />
-            <Icon icon="fa6-solid:circle" />
-            <Icon icon="fa6-solid:circle" />
-        </div>
-    </section>
+  <section class="w-full">
+    <img
+      class=""
+      src="//www.ladbrokes.com.au/cdn/contentful/images/J9Zlbf9VwEuTI8OiSvPEW/b7a4aac66cb484549528906dd4ee1561/LadbrokesLive_MOBILE.jpg"
+    />
+    <div class="flex gap-2 justify-center p-2">
+      <Icon icon="fa6-solid:circle" />
+      <Icon icon="fa6-solid:circle" />
+      <Icon icon="fa6-regular:circle" />
+      <Icon icon="fa6-solid:circle" />
+      <Icon icon="fa6-solid:circle" />
+    </div>
+  </section>
 </template>
 
 <script setup lang="ts">
-    import { Icon } from '@iconify/vue';
+import { Icon } from "@iconify/vue";
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
