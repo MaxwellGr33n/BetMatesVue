a3cd05f89c65aec51ce51bce91cb9f56d0ca096a
Max Green <maxdgreen@live.com.au>
2024-03-22 13:42:09 +1100

    Updated with link and most recent UFC event.

--- src/components/HotEvent.vue
+++ src/components/HotEvent.vue
@@ -1,17 +1,19 @@
 <template>
   <section class="relative py-5 px-3 w-full">
-    <div class="flex items-center justify-center bg-violet-500 rounded-md p-3">
-      <img
-        class="absolute top-5"
-        src="https://www.ladbrokes.com.au/images/ladbrokes/feature-event/featured-event-cta-banner--light.svg"
-        width="120px"
-      />
-      <div class="w-full ml-7">
-        <h2 class="mt-1 text-center text-white font-semibold text-lg">UFC 296 MAIN EVENT:</h2>
-        <p class="text-center text-white">Edwards vs Covington</p>
+    <a href="https://www.ufc.com/" target="_blank">
+      <div class="flex items-center justify-center bg-violet-500 rounded-md p-3">
+        <img
+          class="absolute top-5"
+          src="https://www.ladbrokes.com.au/images/ladbrokes/feature-event/featured-event-cta-banner--light.svg"
+          width="120px"
+        />
+        <div class="w-full ml-7">
+          <h2 class="mt-2 text-center text-white font-semibold text-lg">UFC 300 MAIN EVENT:</h2>
+          <p class="text-center text-white">PEREIRA VS HILL</p>
+        </div>
+        <Icon icon="material-symbols:arrow-forward-ios" height="28px" style="color: white" />
       </div>
-      <Icon icon="material-symbols:arrow-forward-ios" height="28px" style="color: white" />
-    </div>
+    </a>
   </section>
 </template>
 
