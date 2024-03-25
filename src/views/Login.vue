ac430608bacaa0a54dc337b5fb687a73dce44c4d
Max Green <maxdgreen@live.com.au>
2024-03-25 20:20:46 +1100

    Added simple ui and logic implementation for login and registration handling.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,35 @@
 <template>
-  <div></div>
+  <div class="w-full h-12">
+    <h1 class="text-lg">Login</h1>
+    <p v-if="authStore.errored" class="text-red-600 text-xs">{{ authStore.errMsg }}</p>
+    <p><input type="text" placeholder="Email" v-model="email" /></p>
+    <p><input type="password" placeholder="Password" v-model="password" /></p>
+    <p><button @click="authStore.signInWithEmailAndPassword(email, password)">Submit</button></p>
+    <p><button @click="handleGoogleSignIn">Sign in with Google</button></p>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import { useRouter } from "vue-router";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useAuthStore } from "@/stores/authStore";
 
-<style scoped></style>
+const router = useRouter();
+const authStore = useAuthStore();
+const email = ref("");
+const password = ref("");
+
+const handleGoogleSignIn = async () => {
+  await authStore.signInWithGoogle();
+};
+
+onMounted(() => {
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      router.push("/");
+    }
+  });
+});
+</script>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,7 +1,32 @@
 <template>
-  <div></div>
+  <div class="w-full h-12">
+    <h1 class="text-lg">Register</h1>
+    <p v-if="authStore.errored" class="text-red-600 text-xs">{{ authStore.errMsg }}</p>
+    <p><input type="text" placeholder="Email" v-model="email" /></p>
+    <p><input type="password" placeholder="Password" v-model="password" /></p>
+    <p><button @click="authStore.register(email, password)">Submit</button></p>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import { useAuthStore } from "@/stores/authStore";
+import { useRouter } from "vue-router";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+const email = ref("");
+const password = ref("");
+const authStore = useAuthStore();
+const router = useRouter();
+
+onMounted(() => {
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      router.push("/");
+    }
+  });
+});
+</script>
 
 <style scoped></style>
