ad8ac4e29d01335c6e0df517cfd4f7634f0bd572
Max Green <maxdgreen@live.com.au>
2024-03-25 12:29:21 +1100

    Created new routes for handling login and register pages. Included betlsip for later implementation.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Login from '@/views/Login.vue'
+import Register from '@/views/Register.vue'
+import Betslip from '@/views/Betslip.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
+    { path: '/', name: 'home', component: HomeView },
+    { path: '/register', name: 'register', component: Register },
+    { path: '/login', name: 'login', component: Login },
+    { path: '/betslip', name: 'betslip', component: Betslip },
   ]
 })
 
