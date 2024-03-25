4c867a810f53a445f529eea34d8ddc2995f7f093
Max Green <maxdgreen@live.com.au>
2024-03-25 12:27:42 +1100

    Configured firebase in main.ts.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,23 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 import './assets/styles.css'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBi_HU7cdANmhJtd1oPWmq9Ws7vD3MCw5c",
+  authDomain: "betmates-e4e02.firebaseapp.com",
+  projectId: "betmates-e4e02",
+  storageBucket: "betmates-e4e02.appspot.com",
+  messagingSenderId: "1058387916158",
+  appId: "1:1058387916158:web:9f13006203b499ed216f2f"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 const app = createApp(App);
 
