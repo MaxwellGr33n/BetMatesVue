51ac429475568ebcc8a487bed5d829119557ab96
Max Green <maxdgreen@live.com.au>
2024-03-25 20:18:43 +1100

    Cleaned up imports. Added onMount getAuth check to update authStores logged in state. This ensure even if the site is closed and re opened the store knowns the logged in state.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,30 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
-import NavBar from "./components/NavBar.vue";
-import QuickLinks from "./components/QuickLinks.vue";
 import { onMounted } from "vue";
 import { useCurrentEventsStore } from "./stores/currentEventsStore";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useAuthStore } from "./stores/authStore";
+import NavBar from "./components/NavBar.vue";
+import QuickLinks from "./components/QuickLinks.vue";
 
 const currentEventsStore = useCurrentEventsStore();
+const authStore = useAuthStore();
 
 onMounted(async () => {
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      authStore.isLoggedIn = true;
+    } else {
+      authStore.isLoggedIn = false;
+    }
+  });
+
   currentEventsStore.loading = true;
   await currentEventsStore.ApiGetEvents("aussierules_afl");
+
   currentEventsStore.updateFilteredEvents("aussierules_afl");
+
   await currentEventsStore.ApiGetEvents("rugbyleague_nrl");
   await currentEventsStore.ApiGetEvents("soccer_australia_aleague");
   await currentEventsStore.ApiGetEvents("baseball_mlb");
