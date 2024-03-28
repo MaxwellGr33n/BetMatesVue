ab53f8ba6e5a0801562fd230b43413a0fa2658f6
Max Green <maxdgreen@live.com.au>
2024-03-28 13:10:38 +1100

    Changed api key import to work with vercel.

--- src/stores/currentEventsStore.ts
+++ src/stores/currentEventsStore.ts
@@ -57,7 +57,7 @@ export const useCurrentEventsStore = defineStore({
     async ApiGetEventsWithOdds(sport: string) {
       try {
         if (!this.currentEvents[sport]) {
-          const response = await axios.get(`https://api.the-odds-api.com/v4/sports/${sport}/odds?apiKey=${import.meta.env.ODDS_API_KEY}&regions=au&markets=h2h&dateFormat=iso&oddsFormat=decimal&bookmakers=ladbrokes_au%2Cneds%2Csportsbet%2Cunibet%2Ctab`);
+          const response = await axios.get(`https://api.the-odds-api.com/v4/sports/${sport}/odds?apiKey=${process.env.ODDS_API_KEY}&regions=au&markets=h2h&dateFormat=iso&oddsFormat=decimal&bookmakers=ladbrokes_au%2Cneds%2Csportsbet%2Cunibet%2Ctab`);
           this.currentEvents[sport] = response.data;
         }
 
