f0ea0832e46b7788738b32ae6ca4cb82c8c2473b
Max Green <maxdgreen@live.com.au>
2024-03-28 12:53:33 +1100

    Added link to betslip to QuickLinks

--- src/components/QuickLinks.vue
+++ src/components/QuickLinks.vue
@@ -4,15 +4,16 @@
     <div class="flex justify-between items-center w-screen sm:w-4/5 max-w-screen-lg">
       <ul class="flex w-full h-full">
         <li v-for="item in quickLinks" :key="item.id" class="w-1/4 h-full">
-          <a
+          <router-link
             @click="toggleActive(item)"
             :class="['flex flex-col items-center justify-center w-full h-full', item.isClicked ? 'bg-neutral-200 shadow-inner' : 'bg-neutral-50']"
+            :to="item.route || '/'"
           >
             <span class="flex items-center">
               <Icon v-if="item.icon" :icon="item.icon" />
             </span>
             <span class="text-2xs">{{ item.text }}</span>
-          </a>
+          </router-link>
         </li>
       </ul>
     </div>
@@ -29,13 +30,14 @@ interface QuickLinkItem {
   icon: string;
   active: boolean;
   isClicked: boolean;
+  route?: string;
 }
 
 const quickLinks = reactive<QuickLinkItem[]>([
   { id: 1, text: "Next Up", icon: "game-icons:stopwatch", active: false, isClicked: false },
   { id: 2, text: "Racing", icon: "la:horse-head", active: false, isClicked: false },
   { id: 3, text: "ESports", icon: "fa6-solid:computer", active: false, isClicked: false },
-  { id: 4, text: "Notifications", icon: "fa6-regular:bell", active: false, isClicked: false },
+  { id: 4, text: "Betslip", icon: "ic:round-insert-drive-file", active: false, isClicked: false, route: "/betslip" },
   { id: 5, text: "Menu", icon: "radix-icons:hamburger-menu", active: false, isClicked: false },
 ]);
 
