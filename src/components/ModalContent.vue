90bc6b631e4ef46105c51838bee3671b6c753bfe
Max Green <maxdgreen@live.com.au>
2024-03-28 12:52:03 +1100

    Add modal content component for betslip notifications.

---
+++ src/components/ModalContent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="bg-neutral-100 p-[50px] rounded-lg">
+    <h2 class="text-2xl pb-4">{{ title }}</h2>
+    <p class="text-lg">{{ msg }}</p>
+    <div class="flex items-center justify-between mt-10">
+      <button
+        class="bg-white hover:bg-neutral-50 text-sm font-bold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline"
+        @click="notificationStore.closeNotification"
+      >
+        Close
+      </button>
+
+      <RouterLink class="bg-white hover:bg-neutral-50 text-sm font-bold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline" to="/betslip"
+        >Betslip</RouterLink
+      >
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "vue";
+import { RouterLink } from "vue-router";
+import { useNotificationStore } from "@/stores/notificationStore";
+
+const notificationStore = useNotificationStore();
+
+const props = defineProps({
+  title: String,
+  msg: String,
+});
+</script>
+
+<style scoped></style>
