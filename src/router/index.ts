b0a79b6bfa5e2b9b99f2dfeee31ab60c6954c1c8
Max Green <maxdgreen@live.com.au>
2024-03-29 21:54:21 +1100

    Fixed after linting changes.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import Login from '@/views/LoginPage.vue'
-import Register from '@/views/RegisterPage.vue'
-import Betslip from '@/views/BetSlip.vue'
+import LoginPage from '@/views/LoginPage.vue'
+import RegisterPage from '@/views/RegisterPage.vue'
+import BetSlip from '@/views/BetSlip.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'home', component: HomeView },
-    { path: '/register', name: 'register', component: Register },
-    { path: '/login', name: 'login', component: Login },
-    { path: '/betslip', name: 'betslip', component: Betslip },
+    { path: '/register', name: 'register', component: RegisterPage },
+    { path: '/login', name: 'login', component: LoginPage },
+    { path: '/betslip', name: 'betslip', component: BetSlip },
   ]
 })
 
