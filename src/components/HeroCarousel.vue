40c33f2edaa20cba0ac3f8ec5209a3e8df81aa4c
Max Green <maxdgreen@live.com.au>
2024-03-29 21:18:47 +1100

    Removed fake image dot indicators.

--- src/components/HeroCarousel.vue
+++ src/components/HeroCarousel.vue
@@ -1,18 +1,9 @@
 <template>
   <section class="w-full">
     <img class="" src="@/assets/images/Betmates Live Cover.jpg" />
-    <div class="flex gap-2 justify-center p-2">
-      <Icon icon="fa6-solid:circle" />
-      <Icon icon="fa6-solid:circle" />
-      <Icon icon="fa6-regular:circle" />
-      <Icon icon="fa6-solid:circle" />
-      <Icon icon="fa6-solid:circle" />
-    </div>
   </section>
 </template>
 
-<script setup lang="ts">
-import { Icon } from "@iconify/vue";
-</script>
+<script setup lang="ts"></script>
 
 <style scoped></style>
